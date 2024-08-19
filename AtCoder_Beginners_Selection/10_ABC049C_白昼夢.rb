442828e357f70b8c2b1c66446695f621fbeaff9d
monyatto <atsuuu.0925@gmail.com>
2024-08-19 11:41:29 +0900

    2024/08/19

--- AtCoder_Beginners_Selection/10_ABC049C_白昼夢.rb
+++ AtCoder_Beginners_Selection/10_ABC049C_白昼夢.rb
@@ -1,2 +1,20 @@
 # https://atcoder.jp/contests/abs/tasks/arc065_a
 
+s = gets.chomp
+s = s.reverse
+
+words = %w[dream dreamer erase eraser].map(&:reverse)
+
+found = true
+
+while found == true
+  found = false
+  words.each do |word|
+    if s.start_with?(word)
+      s = s[word.length..]
+      found = true
+    end
+  end
+end
+
+puts s.empty? ? 'YES' : 'NO'
